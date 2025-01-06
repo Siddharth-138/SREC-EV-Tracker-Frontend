71f52c3f87cd6c0c6414d17c8edd13572c083d49
Siddharth <146336321+Siddharth-138@users.noreply.github.com>
2025-01-06 22:22:20 +0530

    host1

--- src/components/dashBoard.jsx
+++ src/components/dashBoard.jsx
@@ -129,7 +129,7 @@ function DashBoard() {
     };
     getTrackData();
 
-    const socket = io('https://blueband-back-818859582946.asia-south1.run.app/');
+    const socket = io('https://blueband-back-577523927330.asia-south2.run.app');
 
     socket.on('locationUpdate', updateCarData);
     socket.on('ok', updateCarStatus);
